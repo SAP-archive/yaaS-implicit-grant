cec0033b66f3f5b9c98155ef6f3eb2f77b9ba765
Lukasz Przezorski | hybris <lukasz.przezorski@hybris.com>
2015-03-30 11:46:58 +0200

    Update to v2

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,9 @@
 function myFunction() {
 
-    var address = "https://api.yaas.io/oauth2/v1/";
+    var address = "https://api.yaas.io/hybris/oauth2/v2/";
     var endpoint = "authorize";
     var response_type = "token"; // token or token id_token
-    var client_id = "qmwNbTvEZpGgRtSa5PTBF8HgIXym5Atd";
+    var client_id = "YLGqGPmVpkre8aG3eXHEn6TZCMGez5Ol"; //client id of your application
     var scope = "seller";
 
     window.open(address + endpoint + "?" + "response_type=" + response_type + "&client_id=" + client_id + "&scope=" + scope);
