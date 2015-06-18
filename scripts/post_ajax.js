768ff0c564bb5647649555c46e043078a1d23e0e
Luke <przezorski@gmail.com>
2015-06-18 10:44:52 +0200

    Update post_ajax.js

--- scripts/post_ajax.js
+++ scripts/post_ajax.js
@@ -1,3 +1,15 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
 $(document).ready(function(){
     $("#one_post").click(function(){
         var obj = document.getElementById("post");
