ab6fcfb0c3631c1b884a37d0b5301378e17428b8
osza <piotr.oszenda@outlook.com>
2015-06-26 14:58:23 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -15,8 +15,8 @@ function myFunction() {
     var address = "https://api.yaas.io/hybris/oauth2/v2/";
     var endpoint = "authorize";
     var response_type = "token"; // token or token id_token
-    var client_id = "YLGqGPmVpkre8aG3eXHEn6TZCMGez5Ol"; //client id of your application
-    var scope = "seller+hybris.tenant%3D<projectid>";
+    var client_id = "<clientid>"; //client id of your application
+    var scope = "hybris.category_create";
 
     window.open(address + endpoint + "?" + "response_type=" + response_type + "&client_id=" + client_id + "&scope=" + scope);
 }
