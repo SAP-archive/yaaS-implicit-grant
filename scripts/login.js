ad94da18388cdadcda1e1187ed33cc8aac678227
Oszenda, Piotr <piotr.oszenda@sap.com>
2015-12-02 12:50:16 +0100

    editorial changes

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,12 @@
 /*
- * [y] hybris Platform
- *
- * Copyright (c) 2000-2015 hybris AG
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of hybris
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with hybris.
+ * [y] SAP Hybris
  */
 
 function myFunction() {
 
     var address = "https://api.yaas.io/hybris/oauth2/b1/";
     var endpoint = "authorize";
-    var response_type = "token"; 
+    var response_type = "token";
     var client_id = "<clientid>"; // replace <clientid> with Client ID of your application
     var scope = "hybris.category_create";
 
